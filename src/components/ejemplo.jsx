9c8568b1bdaecb644f16704fdd704013a50fe6b7
Mariano Gauna <Mariano.Gauna@endava.com>
2025-02-27 17:29:59 -0300

    Add guitar cart.

---
+++ src/components/ejemplo.jsx
@@ -0,0 +1,7 @@
+
+import { useState } from "react";
+
+const [customer, setCustomer] = useState({});
+const [total, setTotal] = useState(0);
+const [products, setProducts] = useState([]);
+const [modal, setModal] = useState(false);
